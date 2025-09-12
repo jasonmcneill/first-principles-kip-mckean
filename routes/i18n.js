b7594ad3eb335f9d7ba31d39a7851472f672b930
Jason McNeill <vrtjason@gmail.com>
2025-09-11 18:36:11 -0700

    Debug

--- routes/i18n.js
+++ routes/i18n.js
@@ -44,6 +44,9 @@ function renderPage(req, res) {
     `${pageSlug}.json`
   );
 
+  console.log("pass2Path exists: " + fs.existsSync(pass2Path));
+  console.log("pass2Path:", pass2path);
+
   if (fs.existsSync(templatePath) && fs.existsSync(pass1Path)) {
     try {
       const pass1 = JSON.parse(fs.readFileSync(pass1Path, "utf8"));
