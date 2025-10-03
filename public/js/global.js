36e5cb7520dee9c2b4c5fa3f91ce60c04e4e641f
Jason McNeill <vrtjason@gmail.com>
2025-10-03 14:25:25 -0700

    Localize numbers

--- public/js/global.js
+++ public/js/global.js
@@ -145,6 +145,22 @@ function listenForAudio() {
   });
 }
 
+function localizeNumbers() {
+  const userLocale = navigator.language || "en-US";
+  document
+    .querySelector("tbody.numerals")
+    .querySelectorAll("td")
+    .forEach((el) => {
+      if (isNaN(el.innerText)) {
+        return;
+      }
+
+      const num = parseFloat(el.innerText);
+      const formatted = new Intl.NumberFormat(userLocale).format(num);
+      el.textContent = formatted;
+    });
+}
+
 function addListeners() {
   listenForScriptureClicks();
 
