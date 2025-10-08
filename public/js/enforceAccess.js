836b0002d12e3aeb37d9c12dfcb29583dbefdde4
Jason McNeill <vrtjason@gmail.com>
2025-10-08 11:06:52 -0700

    Enforce access

---
+++ public/js/enforceAccess.js
@@ -0,0 +1,25 @@
+(() => {
+  const redirectToLogin = () => {
+    const lang = document.querySelector("html").getAttribute("lang");
+    const loginUrl = `/${lang}/login`;
+
+    sessionStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
+
+    window.location.href = loginUrl;
+  };
+
+  const refreshTokenStored = localStorage.getItem("refreshToken");
+
+  if (!refreshTokenStored) return redirectToLogin();
+
+  const refreshToken = JSON.parse(atob(refreshTokenStored.split(".")[1]));
+
+  const now = Math.floor(Date.now() / 1000);
+  const isExpired = now >= exp;
+  if (isExpired) return redirectToLogin();
+
+  /* if (!refreshToken.subscribeduntil) return redirectToLogin();
+  const isSubscriptionCurrent = now >= refreshToken.subscribeduntil;
+  if (!isSubscriptionCurrent) return redirectToLogin(); */
+})();
