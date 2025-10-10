d2fccaeff3c17cb708ee64759f046f9657043f2a
Jason McNeill <vrtjason@gmail.com>
2025-10-10 14:42:44 -0700

    Build out final step for resetting password

--- public/js/pw-forgot-new.js
+++ public/js/pw-forgot-new.js
@@ -1,4 +1,127 @@
-function addListeners() {}
+function hideAlert() {
+  const alertEl = document.querySelector("#alert");
+  alertEl.classList.add("d-none");
+}
+
+function showAlert(content, headline) {
+  const alertEl = document.querySelector("#alert");
+  let contentHTML = "";
+
+  if (headline && content) {
+    contentHTML = `
+      <h3 class="my-1">${headline}</h3>
+      ${content}
+    `;
+  } else if (content) {
+    contentHTML = content;
+  }
+
+  alertEl.querySelector(".alert-content").innerHTML = contentHTML;
+  alertEl.classList.remove("d-none");
+  fpScrollTo(alertEl);
+}
+
+function resetErrors() {
+  document
+    .querySelectorAll(".is-invalid")
+    .forEach((item) => item.classList.remove("is-invalid"));
+
+  hideAlert();
+
+  resetSubmitButtons();
+}
+
+async function onSubmit(evt) {
+  evt.preventDefault();
+  resetErrors();
+  showSubmitButtonSpinner(evt);
+
+  const passwordEl = evt.target.querySelector("#password");
+  const password = passwordEl.value.trim();
+
+  if (!passwordEl.value.trim().length) {
+    resetErrors();
+    passwordEl.parentElement.querySelector(".invalid-feedback").innerHTML =
+      getPhrase("errorPasswordRequired");
+    passwordEl.classList.add("is-invalid");
+    passwordEl.parentElement.scrollIntoView();
+    passwordEl.focus();
+    return;
+  }
+
+  if (passwordEl.value.trim().length < 8) {
+    resetErrors();
+    passwordEl.parentElement.querySelector(".invalid-feedback").innerHTML =
+      getPhrase("errorPasswordInvalid");
+    passwordEl.classList.add("is-invalid");
+    passwordEl.parentElement.scrollIntoView();
+    passwordEl.focus();
+    return;
+  }
+
+  const accessToken = await getAccessToken();
+
+  fetch("/api/pw-forgot-new", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      password: password,
+    }),
+    headers: new Headers({
+      "Content-Type": "application/json",
+      authorization: `Bearer ${accessToken}`,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      switch (data.msg) {
+        case "password is required":
+          resetErrors();
+          passwordEl.parentElement.querySelector(
+            ".invalid-feedback"
+          ).innerHTML = getPhrase("errorPasswordRequired");
+          passwordEl.classList.add("is-invalid");
+          passwordEl.parentElement.scrollIntoView();
+          passwordEl.focus();
+          break;
+        case "password must be at least 8 characters":
+          resetErrors();
+          passwordEl.parentElement.querySelector(
+            ".invalid-feedback"
+          ).innerHTML = getPhrase("errorPasswordInvalid");
+          passwordEl.classList.add("is-invalid");
+          passwordEl.parentElement.scrollIntoView();
+          passwordEl.focus();
+          break;
+        case "password updated":
+          resetErrors();
+          document.querySelector("h2.pageHeadline").innerHTML = getPhrase(
+            "titlePasswordUpdated"
+          );
+          document.querySelector("#inputNewPassword").classList.add("d-none");
+          document.querySelector("#passwordUpdated").classList.remove("d-none");
+          break;
+        default:
+          resetErrors();
+          showAlert(getPhrase("errorGeneric"), getPhrase("errorTitle"));
+          passwordEl.focus();
+          console.error(data.msg);
+          break;
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      resetErrors();
+    });
+}
+
+function addListeners() {
+  document
+    .querySelector("#newPasswordForm")
+    .addEventListener("submit", onSubmit);
+}
 
 function init() {
   addListeners();
--- routes/api/pw-forgot-new.js
+++ routes/api/pw-forgot-new.js
@@ -1,11 +1,58 @@
 exports.POST = async (req, res) => {
   const db = require("../../db");
+  const userid = req.user.id;
+  const password = req.body.password || "";
 
-  // TODO:  Validate password (must be 8 digits)
+  // Validate
 
-  // TODO:  Hash password with BCrypt
+  if (!password.trim().length) {
+    return res.status(400).send({
+      msg: "password is required",
+      msgType: "error",
+    });
+  }
 
-  // TODO:  Store password in DB
+  if (password.trim().length < 8) {
+    return res.status(400).send({
+      msg: "password must be at least 8 characters",
+      msgType: "error",
+    });
+  }
 
-  // TODO:  Send success response
+  const saltRounds = 10;
+
+  bcrypt.hash(password, saltRounds, (hashErr, hashedPassword) => {
+    if (hashErr) {
+      console.log(hashErr);
+      return res.status(500).send({
+        msg: "unable to hash password",
+        msgType: "error",
+      });
+    }
+
+    const sql = `
+      UPDATE
+        users
+      SET
+        password = ?
+      WHERE
+        id = ?
+      ;
+    `;
+
+    db.query(sql, [hashedPassword, userid], (error, result) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).send({
+          msg: "unable to update password",
+          msgType: "error",
+        });
+      }
+
+      return res.status(200).send({
+        msg: "password updated",
+        msgType: "success",
+      });
+    });
+  });
 };
