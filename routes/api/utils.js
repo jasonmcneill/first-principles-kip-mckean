cbc6978384bae5d33155804578bb22469ff6d5d9
Jason McNeill <vrtjason@gmail.com>
2025-10-15 16:03:55 -0700

    Remove X-Mailer header

--- routes/api/utils.js
+++ routes/api/utils.js
@@ -142,9 +142,6 @@ function sendMail_MailJet(
           Subject: subject,
           TextPart: textBody,
           HTMLPart: htmlBody,
-          Headers: {
-            "X-Mailer": "fp.kipmckean.com via Mailjet",
-          },
         },
       ],
     });
