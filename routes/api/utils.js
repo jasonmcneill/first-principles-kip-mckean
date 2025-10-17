7c17cd77c5ec9c7d1c3afbdbfe8b697c127354c1
Jason McNeill <vrtjason@gmail.com>
2025-10-17 14:39:18 -0700

    Normalize HTTP response codes from mail vendors

--- routes/api/utils.js
+++ routes/api/utils.js
@@ -93,9 +93,35 @@ function sendMail_MailGun(
           console.log(error);
           reject(error);
         } else {
+          let statusText = "OK";
+          let statusCode = 200;
+
+          if (body && body.message) {
+            if (
+              body.message.includes(
+                "account-requests-per-sec limit exceeded, try again after 120 seconds"
+              )
+            ) {
+              statusCode = 200;
+              statusText = body.message;
+            } else if (body.message.includes("Internal Server Error")) {
+              statusCode = 500;
+              statusText = body.message;
+            } else if (body === "Forbidden") {
+              statusCode = 401;
+              statusText = body;
+            } else if (body.message !== "Queued. Thank you.") {
+              statusCode = 400;
+              statusText = body.message;
+            } else {
+              statusCode = 200;
+              statusText = body.message;
+            }
+          }
+
           const mailResponse = {
-            status: 200,
-            statusText: body.message,
+            status: statusCode,
+            statusText: statusText,
           };
 
           resolve(mailResponse);
