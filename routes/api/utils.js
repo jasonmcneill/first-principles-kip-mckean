7dee04a74375c09ac72adb4fae89906d44d28af6
Jason McNeill <vrtjason@gmail.com>
2025-10-29 22:15:14 -0700

    Revise env names

--- routes/api/utils.js
+++ routes/api/utils.js
@@ -29,8 +29,8 @@ exports.sendMail = (
   subject = "",
   htmlBody = "",
   textBody = "",
-  fromEmailName = process.env.EMAIL_FROM_NAME,
-  fromEmailAddress = process.env.EMAIL_FROM_ADDRESS
+  fromEmailName = process.env.KIPMCKEAN_EMAIL_FROM_NAME,
+  fromEmailAddress = process.env.KIPMCKEAN_EMAIL_FROM_ADDRESS
 ) => {
   return new Promise((resolve, reject) => {
     if (process.env.NODE_ENV === "production") {
@@ -198,12 +198,12 @@ function sendMail_ZeptoMail(
     const fromEmail =
       fromEmailAddress && fromEmailAddress.length
         ? fromEmailAddress
-        : process.env.EMAIL_FROM_ADDRESS;
+        : process.env.KIPMCKEAN_EMAIL_FROM_ADDRESS;
 
     const fromName =
       fromEmailName && fromEmailName.length
         ? fromEmailName
-        : process.env.EMAIL_FROM_NAME;
+        : process.env.KIPMCKEAN_EMAIL_FROM_NAME;
 
     try {
       const mailOptions = {
