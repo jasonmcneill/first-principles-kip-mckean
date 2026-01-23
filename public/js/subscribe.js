051a2a47900a17c20ef415215a38776ae34cc2e5
Jason McNeill <vrtjason@gmail.com>
2026-01-22 17:05:36 -0700

    Check database to confirm whether user is already subscribed

--- public/js/subscribe.js
+++ public/js/subscribe.js
@@ -1,34 +1,67 @@
 function localizePrice() {
-  const priceElement = document.getElementById('price');
+  const priceElement = document.getElementById("price");
   const price = 12.0;
-  const locale = navigator.language || 'en-US';
-  const currency = 'USD';
+  const locale = navigator.language || "en-US";
+  const currency = "USD";
 
   priceElement.textContent = new Intl.NumberFormat(locale, {
-    style: 'currency',
-    currency
+    style: "currency",
+    currency,
   }).format(price);
 }
 
 function paypalButtons() {
-  const planId = document.querySelector("[data-paypal-plan-id]").getAttribute("data-paypal-plan-id");
-  paypal.Buttons({
+  const planId = document
+    .querySelector("[data-paypal-plan-id]")
+    .getAttribute("data-paypal-plan-id");
+  paypal
+    .Buttons({
       style: {
-          shape: 'rect',
-          color: 'white',
-          layout: 'vertical',
-          label: 'subscribe'
+        shape: "rect",
+        color: "white",
+        layout: "vertical",
+        label: "subscribe",
       },
-      createSubscription: function(data, actions) {
-
+      createSubscription: function (data, actions) {
         return actions.subscription.create({
-          plan_id: planId
+          plan_id: planId,
         });
       },
-      onApprove: function(data, actions) {
+      onApprove: function (data, actions) {
         alert(data.subscriptionID);
+      },
+    })
+    .render(`#paypal-button-container-${planId}`);
+}
+
+async function checkSubscription() {
+  const endpoint = "/api/check-subscription";
+  const accessToken = await getAccessToken().catch(() => null);
+
+  if (!accessToken) return;
+
+  fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.msg && data.msg !== "subscription active") {
+        return subscribe();
       }
-  }).render(`#paypal-button-container-${planId}`); 
+
+      if (data.refreshToken) {
+        localStorage.setItem("refreshToken", data.refreshToken);
+      }
+      if (data.accessToken) {
+        sessionStorage.setItem("accessToken", data.accessToken);
+      }
+
+      window.location.replace("./dashboard");
+    });
 }
 
 function init() {
