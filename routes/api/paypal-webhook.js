0813341f8207e08444aa6d8a7e1f9684b7dff484
Jason McNeill <vrtjason@gmail.com>
2026-02-05 11:11:07 -0700

    Fix bug

--- routes/api/paypal-webhook.js
+++ routes/api/paypal-webhook.js
@@ -1,20 +1,20 @@
 exports.POST = async (req, res) => {
-  const db = require("../../db");
+  const db = require('../../db');
 
   // PayPal Webhook Event Body
   const eventBody = req.body;
 
   // Log the subscription webhook event (for debugging purposes)
   console.log(
-    "Received PayPal Webhook Event:",
-    JSON.stringify(eventBody, null, 2),
+    'Received PayPal Webhook Event:',
+    JSON.stringify(eventBody, null, 2)
   );
 
   // Process the subscription webhook event based on its type
   // https://developer.paypal.com/api/rest/webhooks/event-names/#subscriptions
   const eventType = eventBody.event_type;
 
-  if (eventType === "PAYMENT.SALE.COMPLETED") {
+  if (eventType === 'PAYMENT.SALE.COMPLETED') {
     const subscriptionId = eventBody.id;
     const subscriptionTime = eventBody.create_time;
     const validUntil = eventBody.valid_until;
@@ -36,17 +36,15 @@ exports.POST = async (req, res) => {
       [subscriptionId, validUntil, JSON.stringify(req.body), req.user.id],
       (err, result) => {
         if (err) {
-          console.log("Unable to update subscription status:", err);
-          return res
-            .status(500)
-            .send({
-              msg: "unable to update subscription status",
-              msgType: "error",
-            });
+          console.log('Unable to update subscription status:', err);
+          return res.status(500).send({
+            msg: 'unable to update subscription status',
+            msgType: 'error',
+          });
         }
 
-        const sql;
-      },
+        return res.status(200).send();
+      }
     );
   }
 };
