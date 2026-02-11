ec7910e172998604b0be72cc1144ed48d7619a20
Jason McNeill <vrtjason@gmail.com>
2026-02-10 18:04:19 -0700

    Fix bug

--- public/js/enforceSubscription.js
+++ public/js/enforceSubscription.js
@@ -1,19 +1,68 @@
 (() => {
-  const refreshTokenStored = localStorage.getItem("refreshToken");
+  const refreshTokenStored = localStorage.getItem('refreshToken');
 
   if (!refreshTokenStored) return login();
 
-  const refreshToken = JSON.parse(atob(refreshTokenStored.split(".")[1]));
+  const refreshToken = JSON.parse(atob(refreshTokenStored.split('.')[1]));
 
   const subscribe = () => {
-    return window.location.replace("./subscribe");
+    return window.location.replace('./subscribe');
+  };
+
+  const getAccessToken = async () => {
+    let needToRefresh = false;
+    const accessToken = sessionStorage.getItem('accessToken') || '';
+    const now = Math.floor(Date.now().valueOf() / 1000);
+    let expiry = 0;
+
+    if (!accessToken.length) needToRefresh = true;
+
+    try {
+      expiry = Math.floor(JSON.parse(atob(accessToken.split('.')[1])).exp);
+      if (expiry < now) needToRefresh = true;
+    } catch (err) {
+      needToRefresh = true;
+    }
+
+    return new Promise((resolve, reject) => {
+      if (!needToRefresh) return resolve(accessToken);
+      const refreshToken = localStorage.getItem('refreshToken') || '';
+      if (!refreshToken.length) return reject('refresh token missing');
+
+      fetch('/api/refresh-token', {
+        method: 'POST',
+        body: JSON.stringify({
+          refreshToken: refreshToken,
+        }),
+        headers: new Headers({
+          'Content-Type': 'application/json',
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          switch (data.msg) {
+            case 'tokens renewed':
+              const { accessToken, refreshToken } = data;
+              localStorage.setItem('refreshToken', refreshToken);
+              sessionStorage.setItem('accessToken', accessToken);
+              resolve(accessToken);
+              break;
+            default:
+              resolve('could not get access token');
+              break;
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
   };
 
   if (!refreshToken.subscribeduntil) return subscribe();
 
   const now = Math.floor(Date.now() / 1000);
   const expiry = Math.floor(
-    new Date(refreshToken.subscribeduntil).getTime() / 1000,
+    new Date(refreshToken.subscribeduntil).getTime() / 1000
   );
   const isSubscriptionCurrent = now < expiry;
 
@@ -22,7 +71,7 @@
   const INTERVAL_MS = 20000; // 20 seconds
 
   const checkSubscription = async () => {
-    const endpoint = "/api/check-subscription";
+    const endpoint = '/api/check-subscription';
     const accessToken = await getAccessToken().catch(() => null);
 
     if (!accessToken) return;
@@ -32,9 +81,9 @@
 
     try {
       const response = await fetch(endpoint, {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${accessToken}`,
         },
         signal: controller.signal,
@@ -42,24 +91,24 @@
 
       const data = await response.json();
 
-      if (data.msg && data.msg !== "subscription active") {
+      if (data.msg && data.msg !== 'subscription active') {
         return subscribe();
       }
 
       if (data.refreshToken) {
-        localStorage.setItem("refreshToken", data.refreshToken);
+        localStorage.setItem('refreshToken', data.refreshToken);
       }
       if (data.accessToken) {
-        sessionStorage.setItem("accessToken", data.accessToken);
+        sessionStorage.setItem('accessToken', data.accessToken);
       }
 
       clearTimeout(timeoutId);
       setTimeout(checkSubscription, INTERVAL_MS);
     } catch (error) {
-      if (error.name === "AbortError") {
-        console.warn("Fetch timed out. Starting next cycle...");
+      if (error.name === 'AbortError') {
+        console.warn('Fetch timed out. Starting next cycle...');
       } else {
-        console.error("Network error:", error);
+        console.error('Network error:', error);
       }
 
       clearTimeout(timeoutId);
