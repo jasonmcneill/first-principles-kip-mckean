39bf3a0a3ddff6cc75ec4775fc5c57abc7fd08df
Jason McNeill <vrtjason@gmail.com>
2026-04-15 14:09:17 -0700

    Update "Expand" text to make it translatable

--- public/js/global.js
+++ public/js/global.js
@@ -212,7 +212,7 @@ function showScripture(slug) {
         const header = modalEl.querySelector('.modal-title');
         const body = modalEl.querySelector('.modal-body');
         const { display, version, book, chapter, verses } = scriptureObject;
-        let expandText = 'Expand';
+        let expandText = getGlobalPhrase('expand');
         let versesHTML = '';
 
         for (let i = 0; i < verses.length; i++) {
